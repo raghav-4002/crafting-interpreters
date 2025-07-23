d94672322c6f46da79a57af5ea8d11daa2dee980
Raghav Sharma <code.practice17@gmail.com>
2025-07-23 20:59:25 +0530

    First commit: Created basic structure

---
+++ lox/Lox.java
@@ -0,0 +1,59 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox 
+{
+    public static void main(String[] args) throws IOException
+    {
+        /* No more than 1 args are provided */
+        if (args.length > 1)
+        {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        }
+
+        /* Run the script from the file */
+        else if (args.length == 1)
+        {
+            runFile(args[0]);
+        }
+
+        /* Start the interactive session */
+        else
+        {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException
+    {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() throws IOException
+    {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;)
+        {
+            System.out.println("> ");
+            String line = reader.readLine();
+
+            if (line == null)
+            {
+                break;
+            }
+
+            run(line);
+        }
+    }
+}
