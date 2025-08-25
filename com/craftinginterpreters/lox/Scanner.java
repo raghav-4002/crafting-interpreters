eabecdfddad787da9dcf2272f822d59edf06e190
Raghav Sharma <code.practice17@gmail.com>
2025-08-25 11:53:07 +0530

    Added tokenization of literals: numbers and strings

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -82,10 +82,61 @@ class Scanner
                 line++;
                 break;
 
+            case '"':
+                string();
+                break;
+
             default:
+                if (isDigit(c)) {
+                    number();
+                }
+                else {
                 Lox.error(line, "Unexpected character.");
+                }
+
                 break;
         }
+    }
+
+    private boolean isDigit(char c)
+    {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number()
+    {
+        while (isDigit(peek())) advance();
+
+        // Look for a fractional part
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the '.'
+            advance();
+
+            while (isDigit(peek()) advance();
+        }
+
+        addToken(NUMBER, 
+            Double.parseDouble(source.substring(start, current));
+    }
+
+    private void string()
+    {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        // The closing "
+        advance();
+
+        // Trim the surrounding quotes
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
     /* ========== Helper methods ========== */
@@ -110,6 +161,12 @@ class Scanner
         }
 
         return source.charAt(current);
+    }
+
+    private char peekNext()
+    {
+        if (current + 1 >= source.lenght()) return '\0';
+        return source.charAt(current + 1);
     }
 
     private boolean isAtEnd()
