4bbaabcf5762dcaae2efa5cd5bb0bacfae8cca41
Raghav Sharma <code.practice17@gmail.com>
2025-09-06 15:19:51 +0530

    Created basic parser logic for expression, comparison and terms.

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -24,8 +24,25 @@ class Parser
         Expr expr = comparison();
 
         while (match(BANG_EQUAL, EQUAL_EQUAL)) {
-
+            Token operator = previous();
+            Expr right     = comparison();
+            expr = new Expr.Binary(expr, operator, right);
         }
+
+        return expr;
+    }
+
+    private Expr comparison()
+    {
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
     }
 
     private boolean match(TokenType... types)
